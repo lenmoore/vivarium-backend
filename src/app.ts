3ab95d9c59079aace7267aaff5868c2e2488b09c
helenavainmaa <helena.vainmaa@fooddocs.com>
2022-12-18 22:53:50 +0200

    ...build?

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ import https from 'https';
 import config from 'config';
 import * as fs from 'fs';
 
-const port = 8080;
+const port = process.env.PORT || 8080;
 // const port = config.get<number>('port');
 console.log(port);
 const app = express();
