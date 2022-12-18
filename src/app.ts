3513e25f2232c842e0fef5de5b373fe1764e1788
helenavainmaa <helena.vainmaa@fooddocs.com>
2022-12-19 00:54:32 +0200

    ...build?

--- src/app.ts
+++ src/app.ts
@@ -18,40 +18,40 @@ app.use(deserializeUser); // on every single request
 app.use(express.json());
 // use the express-static middleware
 app.use(express.static('public'));
-app.use(
-    cors({
-        origin: [
-            'http://localhost:8080',
-            'https://localhost:8080',
-            'https://192.168.1.153:8080',
-            'http://192.168.1.153:8080',
-            'http://10.0.0.244:8080',
-            'https://192.168.1.153:8080',
-            'http://192.168.8.102:8080',
-            'https://192.168.8.102:8080',
-            'http://192.168.1.153:8080',
-            'https://192.168.1.153:8080',
-            'https://192.168.1.187:8080',
-            'http://192.168.1.187:8080',
-            'https://10.0.0.244:8080',
-            'http://10.0.0.244:8080',
-            'https://172.16.1.206:8080',
-            'http://172.16.1.206:8080',
-            'https://10.0.0.244:8080',
-            'http://10.0.0.244:8080',
-            'https://192.168.237.106:8080',
-            'http://192.168.237.106:8080',
-            'https://192.168.1.166:8080',
-            'https://192.168.237.106:80/',
-            'https://10.0.0.244:80/',
-            'https://vat-vivaarium.herokuapp.com/',
-            '*.herokuapp.com',
-            '*',
-        ],
-        credentials: true,
-    })
-);
-
+// app.use(
+//     cors({
+//         origin: [
+//             'http://localhost:8080',
+//             'https://localhost:8080',
+//             'https://192.168.1.153:8080',
+//             'http://192.168.1.153:8080',
+//             'http://10.0.0.244:8080',
+//             'https://192.168.1.153:8080',
+//             'http://192.168.8.102:8080',
+//             'https://192.168.8.102:8080',
+//             'http://192.168.1.153:8080',
+//             'https://192.168.1.153:8080',
+//             'https://192.168.1.187:8080',
+//             'http://192.168.1.187:8080',
+//             'https://10.0.0.244:8080',
+//             'http://10.0.0.244:8080',
+//             'https://172.16.1.206:8080',
+//             'http://172.16.1.206:8080',
+//             'https://10.0.0.244:8080',
+//             'http://10.0.0.244:8080',
+//             'https://192.168.237.106:8080',
+//             'http://192.168.237.106:8080',
+//             'https://192.168.1.166:8080',
+//             'https://192.168.237.106:80/',
+//             'https://10.0.0.244:80/',
+//             'https://vat-vivaarium.herokuapp.com/',
+//             '*.herokuapp.com',
+//             '*',
+//         ],
+//         credentials: true,
+//     })
+// );
+app.use(cors());
 // const key = fs.readFileSync('./localhost-key.pem');
 // const cert = fs.readFileSync('./localhost.pem');
 // const server = https.createServer({ key: key, cert: cert }, app);
